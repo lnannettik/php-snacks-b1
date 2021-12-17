d324c597f6e048a7bced0a59b14afc52ebaad8eb
lnannettik <l.nannetti.k@gmail.com>
2021-12-17 19:24:20 +0100

    finito 6 brutto

--- snack6.php
+++ snack6.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>snack6</title>
-</head>
-<body>
     
 <!-- // Snack 6
     // Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde.-->
@@ -39,12 +30,48 @@
         ]
     ];
  
+    $db_keys = array_keys($db);
 ?>
 
 
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
     
-</body>    
-</html>
+<div class="container" style="display: flex">
 
+    <div class="teachers" style="width: 300px; height: 300px; background-color: grey; margin: 20px">
+        <?php
+            for ($i= 0; $i < count($db[$db_keys[0]]); $i++) {
+                
+                echo $db[$db_keys[0]] [$i] ['name'];
+                echo $db[$db_keys[0]] [$i] ['lastname'];
+
+            }
+
+        ?>
+    </div>
+
+
+    <div class="pm" style="width: 300px; height: 300px; background-color: green;  margin: 20px">
+        <?php
+            for ($i= 0; $i < count($db[$db_keys[1]]); $i++) {
+                
+                echo $db[$db_keys[1]] [$i] ['name'];
+                echo $db[$db_keys[1]] [$i] ['lastname'];
+
+            }
+        ?>
+    </div>
+
+</div>
+
+
+</body>
+</html>
